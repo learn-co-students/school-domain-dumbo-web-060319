4656c7d13eef0bd721ea2069165888719c1eb695
Alexander Schelchere <ascellie@gmail.com>
2019-06-04 19:55:17 -0400

    We can now add students to our school

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,12 @@ class School
     end
     
     def add_student(student, grade)
-
+        if roster.has_key? grade
+            roster[grade] << student
+        else
+            roster[grade] = []
+            roster[grade] << student
+        end
     end
 
     def grade
