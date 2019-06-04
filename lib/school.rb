7bc8ba25c5a03821db1bd681156930617c764420
Alexander Schelchere <ascellie@gmail.com>
2019-06-04 19:54:53 -0400

    A new instance of school is initialized with an empty roster

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require "pry"
+
+class School
+    attr_reader :roster
+
+    def initialize(name)
+        @name = name
+        @roster = Hash.new
+    end
+    
+    def add_student(student, grade)
+
+    end
+
+    def grade
+
+    end
+    
+    def sort
+
+    end
+end
+
+school = School.new("Bayside High School")
+
+# puts school
+# puts school
