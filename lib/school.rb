b2d86e1600d43cde05212c65d9b973aff7c3ffcd
Garrett Halstein <garrett.halstein@emory.edu>
2019-06-05 16:57:19 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
-# code here!
+require 'pry'
+
+class School 
+  
+  attr_accessor :name, :roster
+  def initialize (name)
+  	@name = name
+  	@roster = {}
+  end
+
+  def grade (grade)
+  	@roster.each {|roster_grade, students|
+  	  if roster_grade == grade
+  	  	return students
+  	  end
+  	}
+  end
+
+  def sort
+  	@roster.each {|roster_grade, students|
+  		@roster[roster_grade] = students.sort
+  	}
+  	@roster
+  end
+
+  def add_student (student_name, grade)
+  	@roster.each {|roster_grade, grade_array| 
+      if grade == roster_grade
+      	@roster[grade] << student_name
+        return
+      end
+    }
+    @roster[grade] = [student_name]
+  end
+end
+
+school = School.new("Bayside High School")
+
